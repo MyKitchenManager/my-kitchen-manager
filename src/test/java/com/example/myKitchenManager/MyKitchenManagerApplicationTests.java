a22571dd7c699b868a2a75379f42d409cd47a602
Chunyang Shen <scy0208@gmail.com>
2020-01-25 10:45:42 -0800

    Add SpringBoot initial files

---
+++ src/test/java/com/example/myKitchenManager/MyKitchenManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myKitchenManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKitchenManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
