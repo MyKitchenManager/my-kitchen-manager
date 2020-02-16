fe8c23a36ea7abb51cf34237c2b6ceff48d855eb
Shanchao Li <shanchao.li@care.com>
2020-02-16 14:56:03 -0800

    Added delete meal plan API

--- src/main/java/com/example/myKitchenManager/repository/MealPlanRepository.java
+++ src/main/java/com/example/myKitchenManager/repository/MealPlanRepository.java
@@ -1,13 +1,25 @@
 package com.example.myKitchenManager.repository;
 
+import com.example.myKitchenManager.entity.Inventory;
 import com.example.myKitchenManager.entity.MealPlan;
 import com.example.myKitchenManager.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface MealPlanRepository extends JpaRepository<MealPlan, Integer>{
-    public MealPlan findByMealPlanId(int mealPlanId);
+    MealPlan findByMealPlanId(int mealPlanId);
+
+    @Modifying
+    @Transactional
+    @Query(
+            value  = "delete from meal_plan where meal_plan.meal_plan_id = ?1",
+            nativeQuery = true
+    )
+    void deleteByMealPlanId(int mealPlanId);
 }
